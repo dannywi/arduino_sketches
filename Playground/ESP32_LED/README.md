01f876de8d3f6f62a4732f735913a0d4d9dccd3e
Danny Widjaya <danny.widjaya@woven-planet.global>
2021-10-20 19:08:24 +0900

    Add new project for ESP32 experiments

---
+++ Playground/ESP32_LED/README.md
@@ -0,0 +1,6 @@
+# ESP32 Scratch PAD - with LED
+A space to test various ESP32's features.
+
+Board spec [here](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/hw-reference/esp32/get-started-devkitc.html).
+
+Made on vscode with PlatformIO plugin
---
+++ Playground/ESP32_LED/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define LED 22
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+uint8_t counter = 0;
+void loop() {
+  Serial.print("HELLO WORLD!!! - ");
+  Serial.println(counter++);
+  if (counter == 255)
+    counter = 0;
+  
+  digitalWrite(LED, HIGH);
+  delay(1500);
+  digitalWrite(LED, LOW);
+  delay(1500);
+}
