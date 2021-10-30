67602a3fc0167a38d012b0bbf3990980d23edbe4
Danny Widjaya <dannywidjaya@DannynoiMac.local>
2021-10-31 08:22:44 +0900

    Add support for lighting up the dot

--- Playground/ESP32_4DIG7SEG/include/XDIG7SEG.h
+++ Playground/ESP32_4DIG7SEG/include/XDIG7SEG.h
@@ -12,71 +12,80 @@
 const size_t SEG_SIZE = 7;
 const size_t DIG_SIZE = 4;
 
-class ESP32_4DIG7SEG {
+class XDIG7SEG {
  public:
   /*
-    - 5641AS connection pins - (S)egment (D)igit:
+    - 5641AS connection pins - (S)egment (D)igit (D)ot(P)in:
         upper:  D1 S1 S6   D2 D3 S2
                  .  .  .    .  .  .
                  .  .  .    .  .  .
-        lower:  S5 S4      S3 S7 D4
+        lower:  S5 S4 DP   S3 S7 D4
     - digits: 1 - 4 left to right
   */
-  ESP32_4DIG7SEG(const std::array<int, SEG_SIZE>& segment_pins,
-                 const std::array<int, DIG_SIZE>& digit_pins)
-      : _segment_pins(segment_pins), _digit_pins(digit_pins) {}
+  XDIG7SEG(const std::array<int, SEG_SIZE>& segment_pins,
+           const std::array<int, DIG_SIZE>& digit_pins, int dot_pin = 0)
+      : _segment_pins(segment_pins),
+        _digit_pins(digit_pins),
+        _dot_pin(dot_pin) {}
 
   void setup() {
-    for (auto pin : _segment_pins) {
-      pinMode(pin, OUTPUT);
-    }
-    for (auto pin : _digit_pins) {
-      pinMode(pin, OUTPUT);
-    }
+    for (auto pin : _segment_pins) pinMode(pin, OUTPUT);
+    for (auto pin : _digit_pins) pinMode(pin, OUTPUT);
+    if (_dot_pin > 0) pinMode(_dot_pin, OUTPUT);
   }
 
+  struct Digit {
+    char c = ' ';
+    bool dot = false;
+  };
+
   // terop the display if more than 4 characters
-  bool set(std::vector<char> digits) const {
+  bool set(std::vector<Digit> digits, int delay_ms = 1000) const {
     if (digits.size() != DIG_SIZE) {
-      std::vector<char> suffix(
-          digits.size() < DIG_SIZE ? DIG_SIZE - digits.size() : 3, ' ');
+      std::vector<Digit> suffix(
+          digits.size() < DIG_SIZE ? DIG_SIZE - digits.size() : 3, Digit());
       digits.insert(digits.end(), suffix.begin(), suffix.end());
     }
 
     // terop
     size_t end_i = DIG_SIZE;
     while (end_i <= digits.size()) {
-      std::array<char, DIG_SIZE> sub_array;
+      std::array<Digit, DIG_SIZE> sub_array;
       auto start_it = digits.begin() + end_i - DIG_SIZE;
       for (size_t i = 0; i < DIG_SIZE; ++i, ++start_it)
         sub_array[i] = *start_it;
 
       ++end_i;
-      const int DELAY = 800;
       unsigned long start_time = millis();
-      while (millis() - start_time < DELAY) set_fixed_len(sub_array);
+      while (millis() - start_time < delay_ms) set_fixed_len(sub_array);
     }
 
     return true;
   }
 
-  bool set(const std::string& str) {
-    Serial.println(str.c_str());
-    std::vector<char> v{str.begin(), str.end()};
-    return set(v);
+  static std::vector<Digit> convert_to_digit(const std::string& str) {
+    std::vector<Digit> v;
+    for (size_t i = 0; i < str.size(); ++i) {
+      Digit d;
+      d.c = str[i];
+      d.dot = i < str.size() - 1 && str[i + 1] == '.';
+      if (d.dot) ++i;
+      v.push_back(d);
+    }
+    return v;
   }
 
  private:
-  bool set_fixed_len(const std::array<char, DIG_SIZE>& digits) const {
+  bool set_fixed_len(const std::array<Digit, DIG_SIZE>& digits) const {
     const static int DELAY = 5;
     for (int i = 0; i < DIG_SIZE; ++i) {
-      set(i, digits[i]);
+      set(i, digits[i].c, digits[i].dot);
       delay(DELAY);
     }
     return true;
   }
 
-  bool set(int digit, char c) const {
+  bool set(int digit, char c, bool dot) const {
     if (digit > 3) return false;
     auto it = dict.find(c);
     if (it == dict.end()) return false;
@@ -87,6 +96,9 @@ class ESP32_4DIG7SEG {
     byte segments = it->second;
     for (int i = 0; i < SEG_SIZE; ++i)
       digitalWrite(_segment_pins[i], (segments & (1 << (7 - i))) ? HIGH : LOW);
+
+    digitalWrite(_dot_pin, (_dot_pin > 0 && dot ? HIGH : LOW));
+
     return true;
   }
 
@@ -155,6 +167,8 @@ class ESP32_4DIG7SEG {
 
   // in order D1 to D4
   std::array<int, DIG_SIZE> _digit_pins;
+
+  int _dot_pin;
 };
 
 #endif
--- Playground/ESP32_4DIG7SEG/src/main.cpp
+++ Playground/ESP32_4DIG7SEG/src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#include "ESP32_4DIG7SEG.h"
+#include "XDIG7SEG.h"
 
 // https://kit.socinno.com/1_3_b/
 
@@ -11,14 +11,15 @@ int pinD = 19;
 int pinE = 21;
 int pinF = 22;
 int pinG = 23;
+int pinDot = 4;
 
 int D1 = 14;
 int D2 = 27;
 int D3 = 26;
 int D4 = 25;
 
-ESP32_4DIG7SEG setter{{pinA, pinB, pinC, pinD, pinE, pinF, pinG},
-                      {D1, D2, D3, D4}};
+XDIG7SEG setter{{pinA, pinB, pinC, pinD, pinE, pinF, pinG},
+                      {D1, D2, D3, D4}, pinDot};
 
 void setup() {
   //Serial.begin(9600);
@@ -26,12 +27,12 @@ void setup() {
 }
 
 void loop() {
-  setter.set("saturday");
-  setter.set("2021-10-29");
-
+  setter.set(setter.convert_to_digit("saturday"));
+  setter.set(setter.convert_to_digit("2021.10.30"));
+  setter.set(setter.convert_to_digit("20.5c"), 3000);
+  setter.set(setter.convert_to_digit("34.8h"), 3000);
   /*
   TODO:
-  - need ability to light up the dots
   - add features, e.g. nice blinking patterns, moving lines, etc.
   */
 }
